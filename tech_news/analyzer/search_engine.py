f5c72f65a277c0a1948e1138204bfbe6c59202bc
Kevillin Santos <kevillin_santos@hotmail.com>
2023-04-22 18:59:51 -0300

    feat: Requisito 07

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,16 @@
+from tech_news.database import search_news
+
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    search = {"title": {"$regex": title, "$options": "i"}}
+    db = search_news(search)
+    result = list()
+
+    for news in db:
+        result.append((news["title"], news["url"]))
+
+    return result
 
 
 # Requisito 8
